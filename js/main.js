c10dbc656841410b6b28d9c3953316eec4a70a53
Soheil <Soheil.Tajahmadi@gmail.com>
2018-10-18 11:21:11 +1100

    updating design and wording

--- js/main.js
+++ js/main.js
@@ -2,11 +2,13 @@ console.log('js connected');
 
 $(document).ready(function() {
 
-
+//scroll smoothly to the next section
   $('a[href*="#"]').on('click', function(e) {
     e.preventDefault();
-    console.log(this);
-    console.log($($(this).attr('href')).offset().top);
     $('html').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'swing');
   });
+
+  //make the vertical bar full page (instead of fill window)
+  let height = $(document).height();
+  $(".vertical").css({height: height +'px'});
 })
